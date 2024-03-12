2e50aa0ecf5033c8b9bdb705e4a70a520bd707a7
Paola Rivas <paoa.rivasm@gmail.com>
2024-03-12 01:53:58 -0400

    Add GET request

--- routes/secret.js
+++ routes/secret.js
@@ -2,20 +2,33 @@ var express = require("express");
 var router = express.Router();
 var messages = require("../sample.json");
 var { generateRandomString } = require("../public/javascripts/helpers.js");
-//import messages from "../sample.json" assert { type: "json" };
 
 /* GET secret */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+router.get("/:secretKey", (req, res) => {
+  try {
+    const { secretKey } = req.params;
+    const secretItem = messages.find((x) => x.secretKey == secretKey);
+    if (!secretItem) {
+      throw new Error("It either never existed or has already been viewed.");
+    }
+    messages.splice(messages.indexOf(secretItem), 1);
+    res.send({ secretMessage: secretItem.message });
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
 });
 
 router.post("/", (req, res) => {
-  const id = messages.length + 1;
-  const { message } = req.body;
-  const secretKey = generateRandomString(7);
-  const newMessage = { id, secretKey, message };
-  messages.push(newMessage);
-  res.json({ secretKey });
+  try {
+    const id = messages.length + 1;
+    const { message } = req.body;
+    const secretKey = generateRandomString(7);
+    const newMessage = { id, secretKey, message };
+    messages.push(newMessage);
+    res.json({ secretKey });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 module.exports = router;
