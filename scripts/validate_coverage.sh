a30307c02120765233d574d7eb282477c7d849d1
Paola Rivas <paoa.rivasm@gmail.com>
2024-03-17 23:51:35 -0400

    Fix error

--- scripts/validate_coverage.sh
+++ scripts/validate_coverage.sh
@@ -2,20 +2,20 @@
 
 echo "Check Coverage"
 
-COVERAGE_REPORT_PATH="../coverage/clover.xml"
+COVERAGE_REPORT_PATH="./coverage/clover.xml"
 
 # Parse XML and extract coverage information
 statements=$(grep -oP '(?<=statements=")[^"]+' $COVERAGE_REPORT_PATH | sed -n '1p')
 covered_statements=$(grep -oP '(?<=coveredstatements=")[^"]+' $COVERAGE_REPORT_PATH | sed -n '1p')
 
 # Calculate coverage percentage
-coverage_percentage=$(awk "BEGIN { print $covered_statements / $statements }")
+coverage_percentage=$(awk "BEGIN { printf \"%.2f\", $covered_statements / $statements }")
 
 # Represents 80% of coverage
 MIN_COVERAGE=0.8
 
 # Check if coverage meets the minimum required
-if [( $(awk "BEGIN {print ($coverage_percentage >= $MIN_COVERAGE) ? 1 : 0}") )]; then
+if [ "$(echo "$coverage_percentage >= $MIN_COVERAGE" | bc)" -eq 1 ]; then
     echo "Pass Successful"
     exit 0
 else
